f330ec81ca781e4f454961b956b083bb9c2871be
jeremy <jeremy.pu@ly.com>
2021-06-19 18:44:33 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pluginOptions: {
+    antd: {
+      importType: 'modularized',
+      style: true
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          javascriptEnabled: true
+        }
+      }
+    }
+  }
+}
