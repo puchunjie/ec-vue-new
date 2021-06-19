d4337fc2d0cdc5ab48579e19a3b912e838c805f0
jeremy.pu <puchunjie@live.com>
2021-06-19 22:10:36 +0800

    feat: new commit

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,18 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'home',
+    component: Home,
+    children: [{
+      path: '/about',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }]
   }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Button>哈哈哈哈</Button>
-  </div>
+  <a-layout class="app-conainer">
+    <a-layout-sider v-model="collapsed" :trigger="null" :width="256" collapsible>
+      <div class="logo" >意施精准塑形普拉提</div>
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-sub-menu key="sub1">
+          <span slot="title"
+            ><a-icon type="setting" /><span>Navigation Four</span></span
+          >
+          <a-menu-item key="1"> Option 7 </a-menu-item>
+          <a-menu-item key="2"> Option 8 </a-menu-item>
+          <a-menu-item key="3"> Option 9 </a-menu-item>
+          <a-menu-item key="5"> Option 10 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"
+            ><a-icon type="setting" /><span>Navigation Four</span></span
+          >
+          <a-menu-item key="7"> Option 7 </a-menu-item>
+          <a-menu-item key="8"> Option 8 </a-menu-item>
+          <a-menu-item key="9"> Option 9 </a-menu-item>
+          <a-menu-item key="10"> Option 10 </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        <router-view />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style lang="less">
+.app-conainer {
+  height: 100%;
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+  .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+    color: #fff;
+    font-size: 16px;
+    text-align: center;
+    line-height: 32px;
+    overflow: hidden;
+    padding: 0 5px;
   }
 }
-</script>
+</style>
